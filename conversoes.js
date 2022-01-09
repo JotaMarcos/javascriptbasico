14c3f010b9a1ab5b5db1cc190c96764265e04071
JotaMarcos <jmarcos_duarte@hotmail.com>
2022-01-09 06:16:53 -0400

    Definição de Variáveis, Booleanos e Conversões

---
+++ conversoes.js
@@ -0,0 +1,29 @@
+// tipo de dados
+// booleanos
+
+// Conversão implícita
+// const numero = 456;
+// const numeroString = "456";
+
+
+// console.log(numero === numeroString);
+// console.log(numero == numeroString);
+// console.log(numero + numeroString);
+
+
+// Conversão explícita
+
+// Number
+// String
+
+const numero = 456;
+const numeroString = "456a";
+
+// O Number converte a String em um número
+console.log(numero + Number(numeroString));
+
+// A String converte um número em uma String
+console.log(String(numero) + numeroString)
+
+
+
---
+++ variaveis.js
@@ -0,0 +1,49 @@
+// Variáveis 
+
+// var
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+
+// console.log("O valor da área é: " + area);
+// var area;
+
+// let
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+
+// if(forma === 'retangulo'){
+    
+//     area = altura *  comprimento;
+// } else {
+//     area = (altura * comprimento) / 2
+// }
+
+// console.log("O valor da área é: " + area);
+
+// const 
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+
+if(forma === 'triangulo'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2
+}
+
+console.log("O valor da área é: " + area);
+
+
+
+
+
+
+
