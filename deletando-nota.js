2a9aae555455dd0ec434966a4b593badf5d70fe8
JotaMarcos <jmarcos_duarte@hotmail.com>
2022-01-09 20:40:08 -0400

    Operações básicas com array

---
+++ deletando-nota.js
@@ -0,0 +1,50 @@
+// // Deletando um elemento no final do array 
+// const notas = [10, 7, 8, 5, 10];
+// notas.pop();
+
+// console.log(notas);
+
+// let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+// //console.log("A média das notas é: " + media);
+// console.log(`A média das notas é: ${media}`);
+
+
+// const arrayVazia = [];
+
+// console.log(arrayVazia)
+// console.log(arrayVazia.length)
+
+
+// const arrayVazia = [];
+// console.log(arrayVazia[0])
+
+
+// const arrayVazia = [,,,];
+// console.log(arrayVazia.length)
+// console.log(arrayVazia[0])
+// console.log(arrayVazia[1])
+// console.log(arrayVazia[2])
+
+
+// const arrayVazia = [,,,];
+// console.log(arrayVazia.length)
+// arrayVazia.push(50)
+// console.log(arrayVazia)
+// console.log(arrayVazia.length)
+
+jantarDeHoje = ['🍔', '🌭', '🍕']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)
+
+
+
+
+
---
+++ media.js
@@ -0,0 +1,54 @@
+// Arrays
+
+// medias 10, 6.5, 8, 7.5
+// let nota1 = 10;
+// let nota2 = 6.5;
+// let nota3 = 8;
+// let nota4 = 7.5;
+
+// let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// const notas = [10, 6.5, 8, 7.5];
+
+// let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+
+// console.log("A média das notas é: " + media);
+
+// const numero1 = 50;
+// const numero2 = 43;
+// const numero3 = 12;
+
+// // utilizando array
+// const numeros = [50, 43, 12];
+
+// console.log(numeros);
+
+
+// Uma lista ordenada de valores enumerados:
+const arrayDeNumeros = [50, 43, 12];
+const arrayDeStrings = ["banana", "alura", "Juliana"];
+
+console.log(arrayDeNumeros);
+console.log(arrayDeStrings);
+
+// array com 1 elemento
+const numero = [5];
+
+// // array com 3 elementos
+// const nums = [50, 43, 12];
+
+console.log(numero);
+// console.log(nums);
+
+// índice     0   1   2   3
+const nums = [50, 43, 12, 98];
+console.log(nums[1]);
+console.log(nums.length)
+
+
+
+
+
+
+
